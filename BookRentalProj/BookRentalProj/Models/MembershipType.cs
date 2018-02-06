1b4b6469797ad3ce07894b3a3385c4297f4cdd66
chiangs <stephen.e.chiang@gmail.com>
2018-02-06 15:06:21 +0100

    init commit

---
+++ BookRentalProj/BookRentalProj/Models/MembershipType.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BookRentalProj.Models
+{
+    public class MembershipType
+    {
+        // Don't forget to add this to the IdentityModels before adding to DB via packageManagerConsole
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [DataType(DataType.Currency)]
+        public byte SignupFee { get; set; }
+
+        [DisplayName("Rental Rate")]
+        [Required]
+        public Byte ChargeRateOneMonth { get; set; }
+
+        [Required]
+        public Byte ChargeRateSixMonth { get; set; }
+    }
+}
+        
---
+++ readme.md
@@ -0,0 +1,24 @@
+# Book Rental Project
+
+## Summary
+This is an ASP.NET MVC 5 course project that simulates a book rental webapp.
+This is a code-first model where the database is created via Nuget PackageManager as migrations to a SQL database managed by MS SQL Server Management Studio.
+
+## Development journey
+
+
+## Functionality
+User can:
+- Administrate:
+
+- Login with:
+
+## Roadmap
+- [x] Book Model
+- [x] MembershipType Model
+- [x] Genre Model
+- [ ] BookRental Model
+- [x] CRUD Genre
+- [x] CRUD Book
+- [x] CRUD MembershipType
+- [ ] CRUD BookRental
